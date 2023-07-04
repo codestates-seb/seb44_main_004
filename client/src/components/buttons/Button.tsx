dfde915d83bdb8d44dc13892d95f145428100c30
jiye-7 <jy522hi@gmail.com>
2023-07-05 02:46:43 +0900

    #5 Button Component
    - primary, subscribe, cancel, publication에 따라 버튼 스타일 분리
    - type 지정이 되지 않은 경우, custom하게 버튼 생성 가능

--- client/src/components/buttons/Button.tsx
+++ client/src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export type ButtonType = 'primary' | 'subscribe' | 'cancel';
+export type ButtonType = 'primary' | 'subscribe' | 'cancel' | 'publication';
 interface ButtonProps {
   type?: ButtonType;
   width?: string;
@@ -12,71 +12,118 @@ interface ButtonProps {
   hoverBackgroundColor?: string;
   borderColor?: string;
   hoverBorderColor?: string;
+  isSubscribed?: boolean;
   onClick?: (e: MouseEvent) => void;
 }
 
-const Button = ({
-  type,
-  width,
-  content,
-  color,
-  backgroundColor,
-  padding,
-  hoverColor,
-  hoverBackgroundColor,
-  borderColor,
-  hoverBorderColor,
-  onClick,
-}: ButtonProps) => {
-  if (!type) {
-    return (
-      <ButtonStyle
-        width={width}
-        color={color}
-        backgroundColor={backgroundColor}
-        padding={padding}
-        hoverColor={hoverColor}
-        hoverBackgroundColor={hoverBackgroundColor}
-        borderColor={borderColor}
-        hoverBorderColor={hoverBorderColor}
-        onClick={onClick}
-      >
-        {content}
-      </ButtonStyle>
-    );
-  } else if (type === 'primary') {
-    return (
-      <ButtonStyle
-        width="6.5rem"
-        color="#3173F6"
-        backgroundColor="#fff"
-        padding="0.7rem"
-        hoverColor="#fff"
-        hoverBackgroundColor="#3173F6"
-        borderColor="#fff"
-        hoverBorderColor="#3173F6"
-        onClick={onClick}
-      >
-        {content}
-      </ButtonStyle>
-    );
-  }
+const Button = (props: ButtonProps) => {
+  const {
+    type,
+    width,
+    content,
+    color,
+    backgroundColor,
+    padding,
+    hoverColor,
+    hoverBackgroundColor,
+    borderColor,
+    hoverBorderColor,
+    isSubscribed,
+    onClick,
+  } = props;
+
+  return (
+    <StyledButton
+      type={type}
+      width={width}
+      color={color}
+      backgroundColor={backgroundColor}
+      padding={padding}
+      hoverColor={hoverColor}
+      hoverBackgroundColor={hoverBackgroundColor}
+      borderColor={borderColor}
+      hoverBorderColor={hoverBorderColor}
+      isSubscribed={isSubscribed}
+      onClick={onClick}
+    >
+      {content}
+    </StyledButton>
+  );
 };
 
-const ButtonStyle = styled.button<ButtonProps>`
+const StyledButton = styled.button<ButtonProps>`
   display: flex;
   justify-content: center;
-  width: ${(props) => (props.width ? `${props.width}` : '6.5rem')};
-  border: 1px solid ${(props) => props.borderColor && props.borderColor};
-  border-radius: 0.5rem;
-  color: ${(props) => (props.color ? props.color : '#fff')};
-  background-color: ${(props) => props.backgroundColor && props.backgroundColor};
-  padding: ${(props) => (props.padding ? props.padding : '0.7rem')};
+  width: ${({ width }) => (width ? `${width}rem` : '6.5rem')};
+  border-radius: ${({ type }) =>
+    type === 'subscribe' || type === 'publication' ? '1.4rem' : '0.5rem'};
+  padding: ${({ padding }) => (padding ? padding : '0.7rem')};
+  color: ${({ color }) => (color ? color : '#fff')};
+  background-color: ${({ backgroundColor }) => backgroundColor && backgroundColor};
+  font-weight: 700;
+  font-size: 1rem;
 
-  &:hover {
-    color: ${(props) => (props.hoverColor ? props.hoverColor : 'red')};
-    background-color: ${({ hoverBackgroundColor }) => hoverBackgroundColor && hoverBackgroundColor};
-  }
+  ${({ type }) =>
+    type === 'primary' &&
+    css`
+      color: ${({ theme }) => theme.colors.mainLogoColor};
+      background-color: ${({ theme }) => theme.colors.mainWhiteColor};
+      border: 0.12rem solid ${({ theme }) => theme.colors.mainLogoColor};
+      transition: transform 0.1s;
+
+      &:hover {
+        color: ${({ theme }) => theme.colors.mainWhiteColor};
+        background-color: ${({ theme }) => theme.colors.mainLogoColor};
+        border: 0.12rem solid ${({ theme }) => theme.colors.mainLogoColor};
+      }
+
+      &:active {
+        transform: scale(0.95);
+      }
+    `}
+
+  ${({ type }) =>
+    type === 'subscribe' &&
+    css`
+      color: ${({ theme }) => theme.colors.mainLogoColor};
+      background-color: transparent;
+      border: 0.12rem solid ${({ theme }) => theme.colors.mainLogoColor};
+    `}
+
+  ${({ isSubscribed }) =>
+    isSubscribed &&
+    css`
+      color: ${({ theme }) => theme.colors.mainWhiteColor};
+      background-color: ${({ theme }) => theme.colors.mainLogoColor};
+      border: 0.12rem solid ${({ theme }) => theme.colors.mainLogoColor};
+    `}
+
+  ${({ type }) =>
+    type === 'cancel' &&
+    css`
+      color: ${({ theme }) => theme.colors.mainLightRed100};
+      background-color: ${({ theme }) => theme.colors.mainWhiteColor};
+      border: 0.12rem solid ${({ theme }) => theme.colors.mainLightRed100};
+      transition: transform 0.1s;
+
+      &:hover {
+        color: ${({ theme }) => theme.colors.mainWhiteColor};
+        background-color: ${({ theme }) => theme.colors.mainLightRed100};
+        border: 0.12rem solid ${({ theme }) => theme.colors.mainLightRed100};
+      }
+
+      &:active {
+        transform: scale(0.95);
+      }
+    `}
+  
+  ${({ type }) =>
+    type === 'publication' &&
+    css`
+      color: ${({ theme }) => theme.colors.mainLogoColor};
+      background-color: transparent;
+      border: 0.12rem solid ${({ theme }) => theme.colors.mainLogoColor};
+    `}
 `;
 
 export default Button;
