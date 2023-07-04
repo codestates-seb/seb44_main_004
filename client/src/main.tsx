39e7f7e6a1cd464ae2e0b6a32a057b2af1f4d579
jiye-7 <62678492+jiye-7@users.noreply.github.com>
2023-07-04 20:49:23 +0900

    공용컴포넌트 tailwindcss, styled-component 설정 (#25)
    * Update issue templates
    
    * Update issue templates
    
    * #5 Input Component
    
    - 공용 Input 컴포넌트
    
    * #5 공용 Label 컴포넌트
    
    * #5 eslint ignore file
    
    * #17 tailwindcss & styled-component, twin.macro
    
    - tailwindcss & styled-component, twin.macro 세팅
    - theme 설정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { store } from './store/store.ts';
+import { theme } from './styles/theme.ts';
+import GlobalStyles from './styles/GlobalStyles.ts';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
