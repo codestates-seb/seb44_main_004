5cb70cb2e7bb020f3f1499e7ddb3b30a985e4f6f
jeongjwon <zlzlsksk111@gmail.com>
2023-07-04 13:50:34 +0900

    feat: 구독 버튼 구현 및 사용자 정보 변경

--- client/src/components/ProfileInfo.tsx
+++ client/src/components/ProfileInfo.tsx
@@ -1,19 +1,45 @@
-import styled from "styled-components";
+import { useState } from "react";
+import { ButtonHTMLAttributes } from 'react';
+import styled  from "styled-components";
 
+import "../index.css";
 
+type ButtonProps = {
+    sub: boolean;
+  } & ButtonHTMLAttributes<HTMLButtonElement>;
 const ProfileInfo = () => {
 
     // 필요한 데이터
     // 닉네임 , 소개글, 구독자 수, 큐레이션 수
-    
-    // width 지정 필요!
-    
+    //받아올 데이터
+    const user:{ email:string, nickName: string, password:string, introduce:string} = {
+        email: "BOOK@gmail.com",
+        nickName: "정지원",
+        password: "12345678",
+        introduce: "안녕하세요. 저는 뿡뿡이입니다.안녕하세요. 저는 뿡뿡이입니다.안녕하세요. 저는 뿡뿡이입니다.안녕하세요. 저는 뿡뿡이입니다."
+        // 프로필 이미지
+    }
+
+    //false : 구독하기 , true : 구독중
+    const [isSubscribe, setIsSubscribe] = useState<boolean>(true);
+
+    //구독 버튼 클릭 핸들러
+    const handleSubscribe = () => {
+        setIsSubscribe(!isSubscribe);
+    }
+
     return(
         
         <ProfileInfoContainer>
             <ProfileInfoLeft>
-                <h2>정지원</h2>
-                <div> 안녕하세요. 저는 책을 좋아하는 책벌레입니다.안녕하세요. 저는 책을 좋아하는 책벌레입니다.안녕하세요. 저는 책을 좋아하는 책벌레입니다.안녕하세요. 저는 책을 좋아하는 책벌레입니다.</div>
+                <div className="user-info">
+                    <h2>{user.nickName}</h2>
+                    {isSubscribe ? 
+                    (<Button sub={isSubscribe} onClick={handleSubscribe}>구독중</Button>) : 
+                    (<Button sub={isSubscribe} onClick={handleSubscribe}>구독하기</Button>)}
+                </div>
+               
+                <div> {user.introduce}</div>
             </ProfileInfoLeft>
 
             <ProfileInfoRight>
@@ -36,15 +62,23 @@ export default ProfileInfo;
 
 const ProfileInfoContainer = styled.section`
     display: flex;
-    /* justify-content: center;
-    gap: 2rem; */
     justify-content: space-between;
     padding: 2rem 0;
     border-bottom: 0.05rem solid black;
     width: 100%;
 `
 const ProfileInfoLeft = styled.div`
+
+    .user-info{
+        display: flex;
+        align-items: center;
+        h2{ 
+            margin-right: 1rem;
+        }
+        
+    }
     >div{
+        margin: 1rem 0;
         width: 20rem;
     }   
 `
@@ -75,3 +109,12 @@ const MyButton = styled.div`
 
     }
 `
+const Button = styled.button<ButtonProps>`
+    border-radius: 1.25rem;
+    padding: 0.3rem 0.5rem;
+    border: 0.1rem solid var(--main-skyBlue-500);
+    background-color: ${(props) => props.sub ? `var(--main-skyBlue-500)` : `white`};
+    color: ${(props) => props.sub ? `white` : `var(--main-skyBlue-500)` };
+
+    cursor: pointer;
+`;
