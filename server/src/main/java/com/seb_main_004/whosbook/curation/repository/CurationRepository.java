ded0231b0e6a6c5c4941bf2ecb7c2cbdc0494094
Kyungju <wwmix@naver.com>
2023-07-05 11:08:26 +0900

    feat: 큐레이션 추가 기능

---
+++ server/src/main/java/com/seb_main_004/whosbook/curation/repository/CurationRepository.java
@@ -0,0 +1,7 @@
+package com.seb_main_004.whosbook.curation.repository;
+
+import com.seb_main_004.whosbook.curation.entity.Curation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurationRepository extends JpaRepository<Curation, Long> {
+}
