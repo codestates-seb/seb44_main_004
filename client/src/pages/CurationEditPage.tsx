10020f0a78c80ead88a3a705a0ab32e6dbf39275
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-06 02:08:08 +0900

    feat: 큐레이션 수정 페이지 스타일링 1차 적용 + 테일윈드

--- client/src/pages/CurationEditPage.tsx
+++ client/src/pages/CurationEditPage.tsx
@@ -1,54 +1,82 @@
+import { useState, useRef, ChangeEvent } from 'react';
+import tw from 'twin.macro';
 import styled from "styled-components";
-import { useState, useRef } from 'react';
+
 import QuillEditor from '../components/quill/QuillEditor';
+import Input from '../components/input/Input';
+import Label from '../components/label/Label';
+import Button from '../components/buttons/Button';
 
 const CurationWritePage = () => {
-  const [curationContent, setCurationContent] = useState("");
+  const [curationContent, setCurationContent] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const quillRef = useRef(null);
 
   return (
+    <><TitleContainer>큐레이션 수정하기</TitleContainer>
     <Container>
-      <MainTitle>큐레이션 수정하기</MainTitle>
       <FormContainer>
-        <Form>
-          <FormLabel>제목</FormLabel>
-          <FormInput placeholder="큐레이션의 제목을 입력해 주세요" />
-        </Form>
-        <Form>
-          <FormLabel>이모지</FormLabel>
-          <FormInput placeholder="큐레이션에 어울리는 이모지를 선택해 주세요" />
-        </Form>
-        <Form>
-            <FormLabel>내용</FormLabel>
-              <QuillEditor quillRef={quillRef} curationContent={curationContent} setcurationContent={setCurationContent} />
-        </Form>
-        <Form>
-          <FormLabel>카테고리</FormLabel>
-          <FormInput placeholder="추천하는 책의 카테고리를 입력해 주세요" />
-        </Form>
-        <Form>
-          <FormLabel>책 정보 등록</FormLabel>
-          <FormInput placeholder="추천하는 책을 검색 후 등록해 주세요" />
-        </Form>
-        <Form>
-          <FormLabel>이미지 첨부</FormLabel>
-          <FileInputContainer>
-            <FileInput
-              type="file"
-              accept="image/jpg, image/png, image/jpeg"
-              id="fileInput"
-            />
-            <FileInputLabel htmlFor="fileInput">이미지를 찾아 업로드 해주세요</FileInputLabel>
-            <FileInputButton htmlFor="fileInput">이미지 찾기</FileInputButton>
-          </FileInputContainer>
-        </Form>
+        <ItemContainer>
+          <Label type="title" htmlFor="title" content="제목" />
+          <Input
+            id="title"
+            placeholder="큐레이션의 제목을 입력해 주세요"
+            width="100%"
+            color="#000"
+            value={inputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} />
+        </ItemContainer>
+        <ItemContainer>
+          <Label type="title" htmlFor="title" content="이모지" />
+          <Input
+            id="emoji"
+            placeholder="큐레이션에 어울리는 이모지를 선택해 주세요"
+            width="100%"
+            color="#000"
+            value={inputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)} />
+        </ItemContainer>
+        <ItemContainer>
+          <Label type="title" htmlFor="title" content="내용" />
+          <Label type="content" htmlFor="content" content="마우스 드래그로 영역을 선택하면 서식을 수정하고, 이미지도 넣을 수 있어요!" />
+          <QuillEditor quillRef={quillRef} curationContent={curationContent} setcurationContent={setCurationContent} />
+        </ItemContainer>
+        <ItemContainer>
+          <Label type="title" htmlFor="title" content="카테고리 선택" />
+          <select id="countries">
+            <option selected>추천하는 책의 카테고리를 선택해 주세요</option>
+            <option value="자기계발">자기계발</option>
+            <option value="경영/경제">경영/경제</option>
+            <option value="가정/육아">가정/육아</option>
+            <option value="수험서">수험서</option>
+          </select>
+        </ItemContainer>
+        <ItemContainer>
+          <Label type="title" htmlFor="title" content="책 정보 등록" />
+          <SearchInputContainer>
+            <SearchInputLabel>추천하는 책을 검색 후 등록해 주세요</SearchInputLabel>
+            <SearchInputButton>책 검색하기</SearchInputButton>
+          </SearchInputContainer>
+        </ItemContainer>
+        <ItemContainer>
+        <Label type="title" htmlFor="title" content="큐레이션 공개 여부" />
+          <RadioButtonContainer>
+            <input type="radio" id="select" name="radio"/>
+            <label htmlFor="select">공개</label>
+            <input type="radio" id="select2" name="radio"/>
+            <label htmlFor="select2">비공개</label>
+          </RadioButtonContainer>
+        </ItemContainer>
         <ButtonContainer>
-          <Button variant="secret">비공개로 발행</Button>
-          <Button variant="cancel">취소</Button>
-          <Button variant="create">발행</Button>
+          <CancelButton>
+            <Button type="cancel" content="취소" />
+          </CancelButton>
+          <PrimaryButton>
+            <Button type="primary" content="발행" />
+          </PrimaryButton>
         </ButtonContainer>
       </FormContainer>
-    </Container>
+    </Container></>
   );
 };
 
@@ -59,70 +87,38 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const MainTitle = styled.div`
+const FormContainer = styled.div`
+  background-color: #EFEFEF;
+  border-radius: 0.3rem;
+  padding: 0rem 3rem 2rem 3rem;
+  width: 40rem;
+  margin-bottom: 8rem;
+`;
+
+const TitleContainer = styled.div`
   margin: 4rem 0rem 3rem 0rem;
   text-align: center;
   font-size: 3rem;
   font-weight: 700;
 `;
 
-const FormContainer = styled.div`
-  background-color: #f8f7f7;
-  border-radius: 0.3rem;
-  padding: 3rem;
-  width: 40rem;
-  margin-bottom: 8rem;
+const ItemContainer = tw.div`
+  bg-inherit
+  flex
+  flex-col
+  mt-12
+  w-full
+  [> label]:mb-3
+  [> button]:mb-3
 `;
 
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 2rem;
-`;
-
-const FormLabel = styled.label`
-  font-weight: 500;
-  margin-bottom: 0.5rem;
-`;
-
-const FormTextArea = styled.textarea`
-  width: 100%;
-  padding: 0.7rem;
-  height: 9rem;
-  font-weight: 100;
-  border: 0.06rem solid #ffffff;
-  border-radius: 0.3rem;
-`;
-
-const FormInput = styled.textarea`
-  width: 100%;
-  padding: 0.7rem;
-  height: 2.5rem;
-  font-weight: 100;
-  border: 0.06rem solid #ffffff;
-  border-radius: 0.3rem;
-`;
-
-const FileInputContainer = styled.div`
+const SearchInputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const FileInput = styled.input`
-  width: 100%;
-  padding: 0.7rem;
-  height: 2.5rem;
-  border: 1px solid #ffffff;
-  background-color: #ffffff;
-  border-radius: 0.3rem;
-  opacity: 0;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
-
-const FileInputLabel = styled.label`
+const SearchInputLabel = styled.label`
   cursor: pointer;
   width: 80%;
   display: block;
@@ -135,7 +131,7 @@ const FileInputLabel = styled.label`
   font-weight: 100;
 `;
 
-const FileInputButton = styled.label`
+const SearchInputButton = styled.label`
   cursor: pointer;
   width: 18%;
   display: block;
@@ -152,39 +148,21 @@ const FileInputButton = styled.label`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 3rem;
 `;
 
-const Button = styled.div<{ variant: string }>`
-  padding: 0.6rem;
-  background-color: #ffffff;
-  color: #3173f6;
-  border-color: #3173f6;
-  border: 0.1rem solid;
-  border-radius: 0.3rem;
-  cursor: pointer;
-  font-weight: 500;
+const CancelButton = styled.div`
+  margin: 10px;
+`;
 
-  ${({ variant }) => {
-    if (variant === "secret") {
-      return `
-        color: #3173f6;
-        border-color: #3173f6;
-        margin-right: 19.4rem;
-      `;
-    } else if (variant === "cancel") {
-      return `
-        color: #fd8f8f;
-        border-color: #fd8f8f;
-        margin-right: 1rem;
-      `;
-    } else if (variant === "create") {
-      return `
-        color: #3173f6;
-        border-color: #3173f6;
-      `;
-    }
-  }}
+const PrimaryButton = styled.div`
+  margin: 10px;
+`;
+
+const RadioButtonContainer = styled.div`
+  margin: 0rem 3rem 3rem -0.3rem;
+  cursor: pointer;
+  font-size: 15px;
+  color: #757575;
 `;
 
 export default CurationWritePage;
