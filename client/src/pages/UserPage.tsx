926fd6b1b83bbf48e1d093d22431593b2bfe2d32
jeongjwon <zlzlsksk111@gmail.com>
2023-07-06 09:34:37 +0900

    feat: 타유저 페이지 레이아웃 구현 완료

---
+++ client/src/pages/UserPage.tsx
@@ -0,0 +1,32 @@
+import tw from "twin.macro";
+import styled from "styled-components";
+
+import ProfileInfo from "../components/ProfileInfo";
+import ProfileDetail from "../components/ProfileDetail";
+
+const UserPage = () => {
+    //구독상태 판별
+    
+
+    return(
+        <UserPageContainer>
+            <ProfileInfo />
+            <ProfileDetail />
+        </UserPageContainer>
+    )
+}
+
+export default UserPage;
+
+
+const UserPageContainer = styled.div`
+    padding: 5rem 10%;
+    background-color: ${({theme}) => theme.colors.mainLightGray100};
+    ${tw`
+        w-full
+        flex
+        flex-col
+        justify-center
+        items-center
+    `} 
+`
