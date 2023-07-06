35d0dafe5da26488dde12e583a4d4b84b1908c02
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-06 16:36:04 +0900

    feat: CurationDetailPage.tsx 생성

---
+++ client/src/pages/CurationDetailPage.tsx
@@ -0,0 +1,13 @@
+import tw from 'twin.macro';
+import styled from "styled-components";
+
+import Input from '../components/input/Input';
+import Label from '../components/label/Label';
+import Button from '../components/buttons/Button';
+
+const CurationDetailPage = () => {
+  return <div></div>
+};
+
+
+export default CurationDetailPage;
