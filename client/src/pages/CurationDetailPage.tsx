154ccd105ce60cb888b3aaa68ee4abcc6e027c9b
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-08 02:58:04 +0900

    feat: 댓글 부분 레이아웃 구현

--- client/src/pages/CurationDetailPage.tsx
+++ client/src/pages/CurationDetailPage.tsx
@@ -5,13 +5,12 @@ import styled from "styled-components";
 import Input from '../components/input/Input';
 import Label from '../components/label/Label';
 import Button from '../components/buttons/Button';
-import CurationProfileInfo from "../components/profiles/CurationProfileInfo";
-import CurationDetailInfo from "../components/profiles/CurationDetailInfo";
+import CurationProfileInfo from '../components/curations/CurationProfileInfo';
+import CurationDetailInfo from '../components/curations/CurationDetailInfo';
+import ReplyProfileInfo from '../components/curations/ReplyProfileInfo';
 
 const CurationDetailPage = () => {
-  const [titleValue, setTitleValue] = useState('');
   const [replyValue, setReplyValue] = useState('');
-  const [replyCountValue, setReplyCountValue] = useState('');
   const currentDate = new Date().toLocaleString('ko-KR', {
     year: 'numeric',
     month: '2-digit',
@@ -24,7 +23,7 @@ const CurationDetailPage = () => {
     <Container>
       <FormContainer>
         <TitleContainer>
-          🌝 나는 앞으로 몇 번의 보름달을 볼 수 있을까
+          🌝 류이치 사카모토가 살아생전 마지막으로 전하는 이야기
         </TitleContainer>
         <GridContainer>
           <DetailInfoLeft>
@@ -35,7 +34,8 @@ const CurationDetailPage = () => {
             <UploadDate>업로드: {currentDate.replace(',', '')}</UploadDate>
           </DetailInfoRight>
         </GridContainer>
-        <ContentContainer>활동가 류이치 사카모토가 살아생전 마지막으로 전하는 이야기. 2020년, 암의 재발과 전이로 인해 치료를 받더라도
+        <ContentContainer>
+          활동가 류이치 사카모토가 살아생전 마지막으로 전하는 이야기. 2020년, 암의 재발과 전이로 인해 치료를 받더라도
           5년 이상 생존율은 50퍼센트라는 진단을 받고서 시간의 유한함에 직면하게 된 류이치 사카모토.
           『나는 앞으로 몇 번의 보름달을 볼 수 있을까』는 그런 그가 삶의 마지막 고비에서 되돌아본 인생과 예술, 우정과 사랑, 자연과 철학, 그리고 시간을 뛰어넘어
           오래도록 기억될 그의 음악과 깊은 사유에 관한 기록이다. 여러 차례 암 수술을 받고 암과 싸우는 것이 아니라, 암과 살아가기”로 마음먹었다고 담담히 당시의 상황을 전하며 시작되는 이야기는
@@ -45,13 +45,11 @@ const CurationDetailPage = () => {
         </ContentContainer>
         <ItemContainer>
           <Label type="title" htmlFor="title" content="추천하는 책" />
-          <Input
-            id="title"
-            width="100%"
-            color="#000"
-            value={titleValue}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setTitleValue(e.target.value)}
-          />
+          {/* <BookInfo/> */}
+          <p>나는 앞으로 몇 번의 보름달을 볼 수 있을까</p>
+          <p>류이치 사카모토</p>
+          <p>위즈덤하우스</p>
+          <p>책 표지</p>
         </ItemContainer>
         <ItemContainer>
           <Label type="title" htmlFor="reply" content="댓글 쓰기" />
@@ -72,19 +70,24 @@ const CurationDetailPage = () => {
           </PrimaryButton>
         </ButtonContainer>
         <ItemContainer>
-          <Label type="title" htmlFor="replycount" content= "댓글 1개" />
-          <Input
-            id="title"
-            width="60%"
-            color="#000"
-            value={replyCountValue}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setReplyCountValue(e.target.value)}
-          />
+          <Label type="title" htmlFor="replycount" content= "댓글 2개" />
+          <CommentContainer>
+            <ReplyProfileInfo/>
+            어쿠스틱과 일렉트로닉, 클래식과 팝 음악의 경계에서 완벽하게 자유로웠던 우리 시대 최고의 마에스트로 최고다!!~~~
+            <UploadDate>{currentDate.replace(',', '')}</UploadDate>
+          </CommentContainer>
+          <CommentContainer>
+            <ReplyProfileInfo/>
+            그가 삶의 마지막 고비에서 되돌아본 인생과 예술, 우정과 사랑, 자연과 철학, 그리고 시간을 뛰어넘는다.
+            그가 삶의 마지막 고비에서 되돌아본 인생과 예술, 우정과 사랑, 자연과 철학, 그리고 시간을 뛰어넘는다.
+            그가 삶의 마지막 고비에서 되돌아본 인생과 예술, 우정과 사랑, 자연과 철학, 그리고 시간을 뛰어넘는다.
+            <UploadDate>{currentDate.replace(',', '')}</UploadDate>
+          </CommentContainer>
         </ItemContainer>
         <ButtonContainer>
-        <PrimaryButton>
+        <DetailButton>
             <Button type="detail" content="더보기" />
-          </PrimaryButton>
+        </DetailButton>
         </ButtonContainer>
       </FormContainer>
     </Container>
@@ -134,7 +137,8 @@ const DetailInfoRight = styled.div`
 `;
 
 const UploadDate = styled.div`
-  margin-bottom: 1rem;
+  margin: .4rem 0rem;
+  color: #ADACAC;
 `;
 
 const ContentContainer = styled.div`
@@ -148,23 +152,40 @@ const ItemContainer = tw.div`
   bg-inherit
   flex
   flex-col
-  mt-16
+  mt-14
   w-full
   [> input]:h-20
-  [> label]:mb-4
+  [> label]:mb-4 
   [> button]:mb-3
+`;
+
+const CommentContainer = styled.div`
+  width: 70%;
+  margin: .4rem 0rem;
+  border-radius: 0.5rem;
+  padding: 1rem 2rem;
+  text-align: left;
+  font-size: 1rem;
+  line-height: 1.6rem;
+  background-color: #F8F7F7;
 `;
 
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 10px;
+  margin-top: .6rem;
+  margin-right: -0.5rem;
 `;
 
 const CancelButton = styled.div`
-  margin: 10px;
+  margin: .6rem;
 `;
 
 const PrimaryButton = styled.div`
-  margin: 10px;
+  margin: .6rem;
+`;
+
+const DetailButton = styled.div`
+  margin-right: 22.7rem;
+  margin-top: .6rem;
 `;
