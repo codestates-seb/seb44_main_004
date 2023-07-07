41c2b7cf279c4d64c984641567ab6a9f8e2ffa64
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-08 04:01:00 +0900

    style: BookInfo에 BookLine 추가

--- client/src/components/curations/BookInfo.tsx
+++ client/src/components/curations/BookInfo.tsx
@@ -7,12 +7,12 @@ const ReplyProfileInfo = () => {
   return (
     <InfoContainer>
       <ProfileInfoLeft>
+        <BookLine/>
         <BookInfoAll>
           <BookImage>
             <DefaultImg src={BookImg} alt="profileImg" />
             <BookInfoContainer>
               <BookTitle>나는 앞으로 몇 번의 보름달을 볼 수 있을까</BookTitle>
-              <BookLine />
               <BookInfo>류이치 사카모토</BookInfo>
               <BookInfo>위즈덤 하우스</BookInfo>
             </BookInfoContainer>
@@ -33,8 +33,12 @@ const InfoContainer = tw.section`
 
 const ProfileInfoLeft = styled.div`
   > div {
-    margin: 1rem 0rem;
+    margin: 0;
   }
+`;
+
+const BookLine = styled.div`
+  border: .01rem solid #D9D9D9;
 `;
 
 const BookInfoAll = tw.div`
@@ -69,17 +73,8 @@ const BookInfoContainer = styled.div`
 const BookTitle = tw.p`
   text-lg
   font-bold
-  mb-6
+  mb-2
   text-[#3173f6]
-`;
-
-const BookLine = styled.div`
-  position: absolute;
-  left: 9.6rem;
-  right: 56.25rem;
-  bottom: 4rem;
-  height: 0.1px;
-  background-color: #D1D5DB;
 `;
 
 const BookInfo = tw.p`
