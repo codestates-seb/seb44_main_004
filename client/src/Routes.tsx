c3d03b344141b17131beb0b72797f6cb81aff111
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-07 18:38:03 +0900

    feat: Routes.tsx에 Curation 관련 Page들 import

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -5,6 +5,10 @@ import SignUp from './pages/User/SignUp';
 import SignIn from './pages/User/SignIn';
 import MyPage from './pages/MyPage';
 import UserPage from './pages/UserPage';
+import CurationWritePage from './pages/CurationWritePage';
+import CurationEditPage from './pages/CurationEditPage';
+import CurationDetailPage from './pages/CurationDetailPage';
+
 
 const RouteProvider = () => {
   return (
@@ -15,6 +19,9 @@ const RouteProvider = () => {
         <Route path="/login" element={<SignIn />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/userpage" element={<UserPage />} />
+        <Route path="/write" element={<CurationWritePage />} />
+        <Route path="/edit" element={<CurationEditPage />} />
+        <Route path="/detail" element={<CurationDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
