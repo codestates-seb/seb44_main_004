c6681da89a36f63c3f8643e1d879845481109d9e
HanJuYoung309 <juyg407@naver.com>
2023-07-08 00:35:29 +0900

    refacotr:댓글도메인 리팩토링

--- server/src/main/java/com/seb_main_004/whosbook/curation/repository/CurationRepository.java
+++ server/src/main/java/com/seb_main_004/whosbook/curation/repository/CurationRepository.java
@@ -4,4 +4,6 @@ import com.seb_main_004.whosbook.curation.entity.Curation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurationRepository extends JpaRepository<Curation, Long> {
+
+    Curation findByCurationId(long curationId);
 }
