044a5f8bab41963391f7f53a573dfb5cda70dc4e
HanJuYoung309 <juyg407@naver.com>
2023-07-11 10:34:21 +0900

    소셜로그인 백단 구현 완료

--- server/src/main/java/com/seb_main_004/whosbook/config/SecurityConfiguration.java
+++ server/src/main/java/com/seb_main_004/whosbook/config/SecurityConfiguration.java
@@ -1,22 +1,27 @@
 package com.seb_main_004.whosbook.config;
 
 
+import com.seb_main_004.whosbook.auth.Oauth.handler.OAuth2MemberSuccessHandler;
 import com.seb_main_004.whosbook.auth.filter.JwtAuthenticationFilter;
 import com.seb_main_004.whosbook.auth.filter.JwtVerificationFilter;
-import com.seb_main_004.whosbook.auth.handler.MemberAccessDeniedHandler;
-import com.seb_main_004.whosbook.auth.handler.MemberAuthenticationEntryPoint;
-import com.seb_main_004.whosbook.auth.handler.MemberAuthenticationFailureHandler;
-import com.seb_main_004.whosbook.auth.handler.MemberAuthenticationSuccessHandler;
+import com.seb_main_004.whosbook.auth.handler.*;
 import com.seb_main_004.whosbook.auth.jwt.JwtTokenizer;
 import com.seb_main_004.whosbook.auth.utils.CustomAuthorityUtils;
+import com.seb_main_004.whosbook.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -32,11 +37,19 @@ public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils authorityUtils;
 
-    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
+    private final MemberService memberService;
+
+    @Value("${spring.security.oauth2.client.registration.google.clientId}")  // (1)
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.clientSecret}") // (2)
+    private String clientSecret;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberService memberService) {
         this.jwtTokenizer = jwtTokenizer;
         this.authorityUtils = authorityUtils;
+        this.memberService = memberService;
     }
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -56,15 +69,36 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll() //요청에대한 권한은 추후에 설정 예정
-                );
+                )
+                .oauth2Login(oauth2-> oauth2.successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer,authorityUtils,memberService)));
         return http.build();
     }
 
-    // (7)
+    // 순환참조에러발생 나므로 static 으로 변경
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+    //ClientRegistration을 저장하기 위한 Responsitory
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        var clientRegistration = clientRegistration();
+
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+
+
+    private ClientRegistration clientRegistration() {
+        //ClientRegistratin 은 OAuth 2 Client에 대한 등록 정보를 표현하는 객체
+        return CommonOAuth2Provider
+                .GOOGLE
+                .getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
+    }
+
 
     // (8)
     @Bean
@@ -75,6 +109,7 @@ public class SecurityConfiguration {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 
@@ -83,7 +118,7 @@ public class SecurityConfiguration {
         public void configure(HttpSecurity builder) throws Exception {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
 
-            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);  // (2-4)
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
             jwtAuthenticationFilter.setFilterProcessesUrl("/login");
 
             jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
@@ -91,8 +126,11 @@ public class SecurityConfiguration {
 
             JwtVerificationFilter jwtVerificationFilter= new JwtVerificationFilter(jwtTokenizer,authorityUtils);
 
+            // JWT 토큰 검증 필터 생성 및 필터 순서 설정 : 인증(일반로그인 or 소셜로그인) 필터 다음에 적용
             builder.addFilter(jwtAuthenticationFilter)
                     .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+            builder.addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
         }
 
 
