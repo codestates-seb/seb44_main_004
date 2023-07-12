0b305253aff5e63978474533a8a5f5821adbddd6
Kyungju <wwmix@naver.com>
2023-07-12 15:23:12 +0900

    add: 주석추가

--- server/src/main/java/com/seb_main_004/whosbook/image/utils/ImageStorageUtils.java
+++ server/src/main/java/com/seb_main_004/whosbook/image/utils/ImageStorageUtils.java
@@ -9,6 +9,8 @@ import java.util.List;
 public class ImageStorageUtils {
     private static final List<String> ACCEPT_IMG_EXTENSION = List.of("jpg", "png", "jpeg", "gif");
     public static void verifyImageExtension(MultipartFile image){
+        // TODO : Apache Tika 같은 라이브러리로 파일 위변조 여부 확인 필요
+
         if (ACCEPT_IMG_EXTENSION.contains(getFileExtension(image)) == false) {
         }
     }
