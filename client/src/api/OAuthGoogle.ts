b9fa497b2e9960bfa5563359cc8d99de91961729
jiye-7 <jy522hi@gmail.com>
2023-07-15 20:26:32 +0900

    #114 사용하지 않는 모듈 삭제, 주석 삭제

--- client/src/api/OAuthGoogle.ts
+++ client/src/api/OAuthGoogle.ts
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { VITE_OAUTH_CLIENT_ID, VITE_OAUTH_GOOGLE_REDIRECT_URL } from '../types/envVariable';
+import { VITE_OAUTH_CLIENT_ID } from '../types/envVariable';
 import { axiosInstance } from './axios';
 
 const temp = <T>(object: any, key: string): object is T => {
@@ -35,12 +34,10 @@ export const client = () => {
     });
   }
 };
-// https://www.googleapis.com/auth/contacts.readonly
 
 /**
  * oauth2SignIn
  */
-// https://c68b-222-110-54-74.ngrok-free.app/login/oauth2/code/google
 export const oauth2SignIn = () => {
   // 액세스 토큰을 요청하기 위한 Google의 OAuth 2.0 엔드포인트
   /* const oauth2EndPoint =  */ axiosInstance
