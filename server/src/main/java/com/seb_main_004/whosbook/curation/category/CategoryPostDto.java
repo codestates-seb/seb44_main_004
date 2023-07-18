4324f6edbabde2f46dd1681332b19feee7ba2354
Kyungju <wwmix@naver.com>
2023-07-18 13:17:44 +0900

    fix: 단일 카테고리 등록 안되는 문제 해결

--- server/src/main/java/com/seb_main_004/whosbook/curation/category/CategoryPostDto.java
+++ server/src/main/java/com/seb_main_004/whosbook/curation/category/CategoryPostDto.java
@@ -4,10 +4,9 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
-
-@Data
 @Builder
+@Data
 public class CategoryPostDto {
-    @NotBlank(message = "카테고리 명을 입력해주세요")
+    @NotBlank(message = "카테고리명을 입력해주세요.")
     private String name;
 }
