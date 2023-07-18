f439446d1141e71a53471e3bee6968986915ceb8
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-18 13:42:16 +0900

    fix: QuillEditor에서 이미지 미리보기 & 업로드 가능하도록 수정

--- client/src/components/quill/QuillEditor.tsx
+++ client/src/components/quill/QuillEditor.tsx
@@ -12,47 +12,46 @@ type QuillEditorProps = {
 const QuillEditor = memo(({ quillRef, contentValue, setContentValue }: QuillEditorProps) => {
   
   const imageHandler = () => {
-  const input = document.createElement("input");
-  const formData = new FormData();
-  const { VITE_AUTH_KEY } = import.meta.env;
-  let url = "";
+    const input = document.createElement("input");
+    const formData = new FormData();
+    const { VITE_AUTH_KEY } = import.meta.env;
   
-  input.setAttribute("type", "file");
-  input.setAttribute("accept", "image/*");
-  input.click();
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
 
-  input.onchange = async () => {
-    const file = input.files;
-    if (file !== null) {
-      formData.append("curationImage", file[0]);
+    input.onchange = async () => {
+      const file = input.files;
+      if (file !== null) {
+        formData.append("curationImage", file[0]);
 
-    try {
-      const response = await axiosInstance.post('http://localhost:8080/curations/images/upload', formData, {
-        headers: {
-          Authorization: `${VITE_AUTH_KEY}`,
-        }
-      });
-      url = response.data.url;
+        try {
+          const response = await axiosInstance.post('http://localhost:8080/curations/images/upload', formData, {
+            headers: {
+              Authorization: `${VITE_AUTH_KEY}`,
+            }
+          });
+          const imageUrl = response.data.imageUrl;
 
-        const range = quillRef.current?.getEditor().getSelection()?.index;
-        if (range !== null && range !== undefined) {
-          const quill = quillRef.current?.getEditor();
-          quill?.setSelection(range, 1);
-          quill?.clipboard.dangerouslyPasteHTML(
-            range,
-            `<img src=${url} alt="curation image tag." />`
-          );
-        }
+          const range = quillRef.current?.getEditor().getSelection()?.index;
+          if (range !== null && range !== undefined) {
+            const quill = quillRef.current?.getEditor();
+            quill?.setSelection(range, 1);
+            quill?.clipboard.dangerouslyPasteHTML(
+              range,
+              `<img src="${imageUrl}" alt="curation image tag." />`
+            );
+          }
           return { ...response, success: true };
         } catch (error) {
-          console.error('error');
+          console.error(error);
         }
       }
     };
   };
 
-const modules = useMemo (
-    () => ( {
+  const modules = useMemo(
+    () => ({
       toolbar: {
         container: [
           [{ header: [1, 2, 3, false] }],
@@ -74,30 +73,30 @@ const modules = useMemo (
     []
   );
 
-
-return (
-	<>
-  <ReactQuill
-          ref={(element) => {
-              if (element !== null) {
-                quillRef.current = element;
-              }
-            }}
-            value={contentValue}
-            onChange={setContentValue}
-            modules={modules}
-            theme="bubble"
-            placeholder="큐레이션의 내용을 입력해 주세요"
-            style={{
-              fontWeight: "100",
-              marginBottom: '0.3rem',
-              backgroundColor: '#f8f7f7',
-              border: 'none',
-              borderRadius: '0.3rem',
-              height: '18.75rem',
-            }}
-          />
-	</>
-)})
+  return (
+    <>
+      <ReactQuill
+        ref={(element) => {
+          if (element !== null) {
+            quillRef.current = element;
+          }
+        }}
+        value={contentValue}
+        onChange={setContentValue}
+        modules={modules}
+        theme="bubble"
+        placeholder="큐레이션의 내용을 입력해 주세요"
+        style={{
+          fontWeight: "100",
+          marginBottom: '0.3rem',
+          backgroundColor: '#f8f7f7',
+          border: 'none',
+          borderRadius: '0.3rem',
+          height: '18.75rem',
+        }}
+      />
+    </>
+  );
+});
 
 export default QuillEditor;
