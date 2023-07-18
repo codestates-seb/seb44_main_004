89a35c1fe87a9cbcac1a85d90e6283ee7b75ccff
Kyungju <122109213+Kyunju@users.noreply.github.com>
2023-07-18 17:28:45 +0900

    feat: 큐레이션 작성시 책정보 저장 및 조회 기능 구현 (#199)

---
+++ server/src/main/java/com/seb_main_004/whosbook/book/entity/Book.java
@@ -0,0 +1,36 @@
+package com.seb_main_004.whosbook.book.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String authors;
+    @Column(nullable = false)
+    private String publisher;
+    private String thumbnail;
+    private String url;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    public Book(String title, String authors, String publisher, String thumbnail, String url, String isbn) {
+        this.title = title;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.thumbnail = thumbnail;
+        this.url = url;
+        this.isbn = isbn;
+    }
+}
