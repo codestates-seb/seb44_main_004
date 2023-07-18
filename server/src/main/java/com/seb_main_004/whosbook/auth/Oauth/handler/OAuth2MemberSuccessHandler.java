5fddb6f5c139444bc0eaa49634002b9978f5a328
HanJuYoung309 <juyg407@naver.com>
2023-07-18 13:39:58 +0900

    fix:소셜로그인 테스트

--- server/src/main/java/com/seb_main_004/whosbook/auth/Oauth/handler/OAuth2MemberSuccessHandler.java
+++ server/src/main/java/com/seb_main_004/whosbook/auth/Oauth/handler/OAuth2MemberSuccessHandler.java
@@ -6,6 +6,7 @@ import com.seb_main_004.whosbook.auth.dto.SocalLoginResponseDto;
 import com.seb_main_004.whosbook.auth.jwt.JwtTokenizer;
 import com.seb_main_004.whosbook.auth.utils.CustomAuthorityUtils;
 import com.seb_main_004.whosbook.member.entity.Member;
+import com.seb_main_004.whosbook.member.repository.MemberRepository;
 import com.seb_main_004.whosbook.member.service.MemberService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -19,10 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
@@ -30,10 +28,13 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
     private final CustomAuthorityUtils authorityUtils;
     private final MemberService memberService;
 
-    public OAuth2MemberSuccessHandler(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberService memberService) {
+    private final MemberRepository memberRepository;
+
+    public OAuth2MemberSuccessHandler(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberService memberService, MemberRepository memberRepository) {
         this.jwtTokenizer = jwtTokenizer;
         this.authorityUtils = authorityUtils;
         this.memberService = memberService;
+        this.memberRepository = memberRepository;
     }
 
     @Override
@@ -45,11 +46,12 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
         List<String> authorities = authorityUtils.createRoles(email);           // 권한 정보 생성
         String imgURL = String.valueOf(oAuth2User.getAttributes().get("picture"));
 
-        String isUsedEmail= authentication.getPrincipal().toString();
+
+        Optional<Member> findEmail = memberRepository.findByEmail(email);
 
 
         //회원정보가 존재할경우 로그인 처리
-        if(email.equals(isUsedEmail)){
+        if(findEmail.isPresent()){
 
             redirect(request, response, email, authorities);  //액세스토큰, 리프레시 토큰을 생성후 프론트에 전달하기 위해 리다이렉트
 
