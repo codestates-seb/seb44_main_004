da36e284b5fb8120814a98626cf5fdb85f29a278
Kyungju <wwmix@naver.com>
2023-07-19 11:09:52 +0900

    fix: list import

--- server/src/main/java/com/seb_main_004/whosbook/curation/repository/CurationRepository.java
+++ server/src/main/java/com/seb_main_004/whosbook/curation/repository/CurationRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CurationRepository extends JpaRepository<Curation, Long> {
 
     Curation findByCurationId(long curationId);
