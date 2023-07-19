18bea81d6fab9aaab53959818b8a87d1abb9aff9
Kyungju <122109213+Kyunju@users.noreply.github.com>
2023-07-20 01:47:46 +0900

    fix: 회원가입시 토큰 (#229)

--- server/src/main/java/com/seb_main_004/whosbook/member/controller/MemberController.java
+++ server/src/main/java/com/seb_main_004/whosbook/member/controller/MemberController.java
@@ -71,11 +71,11 @@ public class MemberController {
                                            Authentication authentication) {
         Member member = memberService.createGoogleMember02(memberMapperClass.socialMemberPostDtoToMember(memberPostDto), memberImage);
 
-        var oAuth2User = (OAuth2User)authentication.getPrincipal();
-        String email = String.valueOf(oAuth2User.getAttributes().get("email"));
+//        var oAuth2User = (OAuth2User)authentication.getPrincipal();
+//        String email = String.valueOf(oAuth2User.getAttributes().get("email"));
 
-        String accessToken=delegateAccessToken(email, member.getRoles());
-        String refreshToken=delegateRefreshToken(email);
+        String accessToken=delegateAccessToken(member.getEmail(), member.getRoles());
+        String refreshToken=delegateRefreshToken(member.getEmail());
         response.setHeader("access_token","Bearer "+accessToken);
         response.setHeader("refresh_token",refreshToken);
 
