455ca0f1b8af57daa0ea05e066fb0a8277d8ad06
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2023-07-20 09:06:36 +0900

    단일 상세 페이지 - 댓글 CRUD 기능 구현 (#217)
    * feat: 댓글 CRUD 기능 구현(페이지네이션, nickname 수정 필요)
    
    * fix: 수정, 더보기 기능
    
    * fix: 속성값으로 닉네임, 이미지 추가
    
    * feat: 책 정보 추가
    
    * fix: replyapi 추가
    
    * fix: 리팩토링
    
    * fix: 페이지네이션 수정
    
    * fix: pr 반영

--- client/src/components/replies/ReplyProfileInfo.tsx
+++ client/src/components/replies/ReplyProfileInfo.tsx
@@ -1,24 +1,55 @@
 import tw from 'twin.macro';
-import styled  from "styled-components";
+import styled from 'styled-components';
 
 import ProfileImg from '../../img/profile_img2.png';
-
-const ReplyProfileInfo = () => {
-    return(
-        <ProfileInfoContainer>
-            <UserInfo>
-                <ProfileImage>
-                    <DefaultImg src={ProfileImg} alt="profileImg" />
-                </ProfileImage>
-                <Nickname>
-                    최연수
-                </Nickname>
-            </UserInfo>
-        </ProfileInfoContainer>
-    )
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { HiPencil } from 'react-icons/hi';
+import { HiTrash } from 'react-icons/hi';
+interface ReplyProfileInfoProp {
+  replierId: number;
+  replyId: number;
+  nickname: string;
+  image?: string | null;
+  content: string;
+  handleCommentEdit: (content: string) => void;
+  handleCommentDelete: (replyId: number) => void;
 }
+const ReplyProfileInfo = ({
+  replierId,
+  replyId,
+  nickname,
+  image,
+  content,
+  handleCommentEdit,
+  handleCommentDelete,
+}: ReplyProfileInfoProp) => {
+  const { memberId } = useSelector((state: RootState) => state.user);
+  return (
+    <ProfileInfoContainer>
+      <UserInfo>
+        <ProfileImage>
+          {/* TODO: 이미지 속성값으로 받아온다면 대체하기 */}
+          <DefaultImg src={ProfileImg} alt="profileImg" />
+        </ProfileImage>
+        <Nickname>{nickname}</Nickname>
+      </UserInfo>
+      <ButtonZone>
+        {memberId === replierId && (
+          <>
+            <EditButton onClick={() => handleCommentEdit(content)}>
+              <HiPencil size="1.5rem" />
+            </EditButton>
+            <DelteButton onClick={() => handleCommentDelete(replyId)}>
+              <HiTrash size="1.5rem" />
+            </DelteButton>
+          </>
+        )}
+      </ButtonZone>
+    </ProfileInfoContainer>
+  );
+};
 export default ReplyProfileInfo;
-
 
 const ProfileInfoContainer = tw.section`
     w-full
@@ -32,22 +63,32 @@ const UserInfo = tw.div`
 `;
 
 const ProfileImage = styled.div`
-    ${tw`
+  ${tw`
         rounded-full
         w-8
         h-8
         mr-3
         mb-2
-    `}  
+    `}
 `;
 
 const DefaultImg = styled.img`
-    height: inherit;
-    padding-left: 0.2rem;
+  height: inherit;
+  padding-left: 0.2rem;
 `;
 
 const Nickname = tw.p`
     text-xl
     font-thin
     mb-2
+`;
+const ButtonZone = tw.div`
+    flex
+    gap-[1rem]
+`;
+const EditButton = tw.button`
+    cursor-pointer
+`;
+const DelteButton = tw.button`
+    cursor-pointer
 `;
