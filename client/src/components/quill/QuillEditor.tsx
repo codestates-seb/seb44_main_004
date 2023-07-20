71429dc4f1f3e20da7d4e18e5f89e35ecd21a0e5
Ella Yeonsu Choi <123397411+ella-yschoi@users.noreply.github.com>
2023-07-20 13:11:30 +0900

    [Client] Quill Editor에 이미지 업로드 시 URL로 반영 (#235)
    * fix: QuillEditor의 image url 통신 부분 수정
    
    * fix: QuillEditor에서 이미지 미리보기 & 업로드 가능하도록 수정
    
    * feat: CurationDetailPage 머지 전 미리 반영
    
    * style: GlobalStyles에 !important 추가
    
    * feat:
    
    QuillEditor에 formats 추가
    
    * feat: Best/New curation 브랜치 변경사항 함께 반영

--- client/src/components/quill/QuillEditor.tsx
+++ client/src/components/quill/QuillEditor.tsx
@@ -1,55 +1,111 @@
+import { MutableRefObject, useMemo, memo } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
-import { MutableRefObject, memo, useMemo } from 'react';
+import { axiosInstance } from '../../api/axios';
 
 type QuillEditorProps = {
-  quillRef: MutableRefObject<unknown>;
+  quillRef: MutableRefObject<ReactQuill | null>;
   contentValue: string | undefined;
   setContentValue: (content: string) => void;
 };
 
-const QuillEditor = memo(({ quillRef, contentValue, setContentValue }: QuillEditorProps) => {
-  const modules = useMemo(
+const QuillEditor = memo (({ quillRef, contentValue, setContentValue }: QuillEditorProps) => {
+  
+  const imageHandler = () => {
+    const input = document.createElement("input");
+    const formData = new FormData();
+    const { VITE_AUTH_KEY } = import.meta.env;
+  
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+
+    input.onchange = async () => {
+      const file = input.files;
+      if (file !== null) {
+        formData.append("curationImage", file[0]);
+
+        try {
+          const response = await axiosInstance.post('http://localhost:8080/curations/images/upload', formData, {
+            headers: {
+              Authorization: `${VITE_AUTH_KEY}`,
+            }
+          });
+          const imageUrl = response.data.imageUrl;
+
+          const range = quillRef.current?.getEditor().getSelection()?.index;
+          if (range !== null && range !== undefined) {
+            const quill = quillRef.current?.getEditor();
+            quill?.setSelection(range, 1);
+            quill?.clipboard.dangerouslyPasteHTML(
+              range,
+              `<img src="${imageUrl}" alt="curation image tag." />`
+            );
+          }
+          return { ...response, success: true };
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+  };
+
+  const modules = useMemo (
     () => ({
       toolbar: {
         container: [
-          [{ header: [1, 2] }],
+          ["image"],
+          [{ header: [1, 2, 3, false] }],
           ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ color: [] }],
           [
             { list: "ordered" },
             { list: "bullet" },
             { indent: "-1" },
             { indent: "+1" },
           ],
-          ["link"],
-          ["image"],
         ],
+        handlers: {
+          image: imageHandler,
+        },
       },
     }),
-    []
-  );
+    []);
+
+    const formats = [
+      'header',
+      'bold',
+      'italic',
+      'underline',
+      'strike',
+      'blockquote',
+      'image',
+    ];
 
   return (
-    <ReactQuill
-      ref={(element) => {
-        if (element !== null) {
-          quillRef.current = element.getEditor();
-        }
-      }}
-      value={contentValue}
-      onChange={setContentValue}
-      modules={modules}
-      theme= "bubble"
-      placeholder="큐레이션의 내용을 입력해 주세요"
-      style={{
-        fontWeight: "100",
-        marginBottom: '0.3rem',
-        backgroundColor: '#f8f7f7',
-        border: 'none',
-        borderRadius: '0.3rem',
-        height: '18.75rem',
-      }}
-    />
+    <>
+      <ReactQuill
+        ref={(element) => {
+          if (element !== null) {
+            quillRef.current = element;
+          }
+        }}
+        value={contentValue}
+        onChange={setContentValue}
+        modules={modules}
+        formats={formats}
+        theme="bubble"
+        placeholder="큐레이션의 내용을 입력해 주세요"
+        style={{
+          fontWeight: "100",
+          marginBottom: '0.3rem',
+          backgroundColor: '#f8f7f7',
+          border: 'none',
+          borderRadius: '0.3rem',
+          height: '18.75rem',
+        }}
+      />
+    </>
   );
 });
 
