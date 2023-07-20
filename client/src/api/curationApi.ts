dab7407765b5ecc72e0ee97f1370deef8a7e0abe
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-07-20 11:40:32 +0900

    feat: curationApi 타입, value 추가

--- client/src/api/curationApi.ts
+++ client/src/api/curationApi.ts
@@ -1,21 +1,36 @@
 import { axiosInstance } from './axios';
 
 // Best Curation Page API (좋아요 수가 많은 순으로 정렬)
-export const mostLikedCurationAPI = async () => {
+export const LikedCurationAPI = async (page: number, size: number) => {
   try {
-    const {
-      data: { data },
-    } = await axiosInstance.get('/curations/new?page=1&size=9');
-    return data;
+    return await axiosInstance.get(`/curations/best?&page=${page}&size=${size}`);
   } catch (error) {
+    console.error(error);
+  }
+}
+
+// Best Curation Page + Category API (좋아요 수가 많은 순으로 정렬 + 카테고리 선택)
+export const LikedCurationCategoryAPI = async (page: number, size: number, category_id: number) => {
+  try {
+    return await axiosInstance.get(`/curations/best?&page=${page}&size=${size}&category=${category_id}`);
+    } catch (error) {
     console.error(error);
   }
 };
 
 // New Curation Page API (가장 최근에 등록된 순으로 정렬)
-export const newlyRegisteredCurationAPI = async () => {
+export const newlyCurationAPI = async (page: number, size: number) => {
   try {
-    return await axiosInstance.get('/curations/new?page=1&size=9');
+    return await axiosInstance.get(`/curations/new?&page=${page}&size=${size}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// New Curation Page + Category API (가장 최근에 등록된 순으로 정렬 + 카테고리 선택)
+export const newlyCurationCategoryAPI = async (page: number, size: number, category_id: number) => {
+  try {
+    return await axiosInstance.get(`/curations/new?&page=${page}&size=${size}&category=${category_id}`);
   } catch (error) {
     console.error(error);
   }
