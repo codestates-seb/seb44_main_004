70c8e22b3152a396984873c2d90c9aa5bca1f81b
Kyungju <wwmix@naver.com>
2023-07-21 14:28:02 +0900

    fix: 만료된 토큰으로 접근 응답

--- server/src/main/java/com/seb_main_004/whosbook/member/controller/MemberController.java
+++ server/src/main/java/com/seb_main_004/whosbook/member/controller/MemberController.java
@@ -23,6 +23,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
@@ -100,7 +101,15 @@ public class MemberController {
 
     //타 유저 마이페이지 조회
     @GetMapping("/{member-id}")
-    public ResponseEntity getOtherMemberPage(@Valid @PathVariable("member-id") long otherMemberId) {
+    public ResponseEntity getOtherMemberPage(@Valid @PathVariable("member-id") long otherMemberId, HttpServletRequest request) {
+        Exception exception = (Exception) request.getAttribute("exception");
+
+        if (exception != null) {
+            if (exception.getMessage().contains("JWT expired")){
+                throw new BusinessLogicException(ExceptionCode.JWT_EXPIRED);
+            }
+        }
+
         Member otherMember = memberService.findVerifiedMemberByMemberId(otherMemberId);
 
         //비회원이 조회할 때
