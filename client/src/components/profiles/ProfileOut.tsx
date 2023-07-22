60bfac74a72ed8d2db54e35f3285863ffb107b58
jeongjwon <zlzlsksk111@gmail.com>
2023-07-23 04:01:46 +0900

    refactor: 회원 탈퇴 문구 및 css 수정

--- client/src/components/profiles/ProfileOut.tsx
+++ client/src/components/profiles/ProfileOut.tsx
@@ -35,7 +35,7 @@ const ProfileOut = () => {
       <Info>
         <Point>{myInfo.mySubscriber}</Point> 명의 구독자를 보유하고 계세요.
       </Info>
-      <Info>최고의 큐레이터가 되는 것을 포기하시겠어요?</Info>
+      <Info>BEST 큐레이터가 되는 것을 포기하시겠어요?</Info>
       <ButtonZone>
         <Button type="detail" content="포기하고 탈퇴하기" width="10rem" onClick={handleOut} />
         <Button type="detail" content="포기하지 않고 도전하기" width="10rem" onClick={handleBack} />
@@ -50,17 +50,16 @@ const ProfileOutContainer = styled.div`
   align-items: center;
 `;
 const Point = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.mainLogoColor};
   padding-right: 1rem;
 `;
 const Info = styled.div`
   display: flex;
-  align-items: flex-end;
-  font-size: 1.5rem;
-
-  margin: 1rem;
+  align-items: center;
+  font-size: 1rem;
+  margin: 0.8rem;
   &:nth-last-child(2) {
     margin-top: 3rem;
     margin-bottom: 2rem;
@@ -80,6 +79,8 @@ const ButtonZone = styled.div`
     background-color: black;
     color: white;
     &:hover {
+      background-color: white;
+      color: black;
     }
   }
 `;
