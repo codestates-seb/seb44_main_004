fe25fbee8746ced4adf8a0dde59af3968dedfcc5
Ella Yeonsu Choi <123397411+ella-yschoi@users.noreply.github.com>
2023-07-23 20:29:43 +0900

    [Client] Quill Editor 기능 추가 및 버그 수정 (#270)
    * fix: Quill 테마 변경
    
    * fix: GlobalStyle에 CSS reset 삭제
    
    * fix: QuillEditor 테마 snow로 변경 및 스타일링 적용
    
    * feat: 이미지 복사, 붙여넣기 금지 기능 추가 및 2MB 이하 제한
    
    * fix: 텍스트 여러번 붙여넣기 가능하도록 버그 픽스

--- client/src/components/quill/QuillEditor.tsx
+++ client/src/components/quill/QuillEditor.tsx
@@ -1,7 +1,8 @@
-import { MutableRefObject, useMemo, memo } from 'react';
+import { MutableRefObject, useMemo, memo, useEffect } from 'react';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.bubble.css';
+import 'react-quill/dist/quill.snow.css';
 import { axiosInstance } from '../../api/axios';
+import { customAlert } from '../../components/alert/sweetAlert';
 
 type QuillEditorProps = {
   quillRef: MutableRefObject<ReactQuill | null>;
@@ -10,47 +11,62 @@ type QuillEditorProps = {
 };
 
 const QuillEditor = memo(({ quillRef, contentValue, setContentValue }: QuillEditorProps) => {
-  const imageHandler = () => {
+  const imageHandler = async () => {
     const input = document.createElement('input');
     const formData = new FormData();
     const { VITE_AUTH_KEY } = import.meta.env;
-
+  
     input.setAttribute('type', 'file');
     input.setAttribute('accept', 'image/*');
     input.click();
-
-    input.onchange = async () => {
-      const file = input.files;
-      if (file !== null) {
-        formData.append('curationImage', file[0]);
-
-        try {
-          const response = await axiosInstance.post(
-            'http://localhost:8080/curations/images/upload',
-            formData,
-            {
-              headers: {
-                Authorization: `${VITE_AUTH_KEY}`,
-              },
-            }
-          );
-          const imageUrl = response.data.imageUrl;
-
-          const range = quillRef.current?.getEditor().getSelection()?.index;
-          if (range !== null && range !== undefined) {
-            const quill = quillRef.current?.getEditor();
-            quill?.setSelection(range, 1);
-            quill?.clipboard.dangerouslyPasteHTML(
-              range,
-              `<img src="${imageUrl}" alt="curation image tag." />`
-            );
-          }
-          return { ...response, success: true };
-        } catch (error) {
-          console.error(error);
+  
+    const file = await new Promise<File | null>((resolve) => {
+      input.onchange = () => {
+        if (input.files !== null) {
+          resolve(input.files[0]);
+        } else {
+          resolve(null);
         }
+      };
+    });
+
+    const maxSize = 2 * 1024 * 1024;
+    if (file && file.size > maxSize) {
+      customAlert({
+        title: '이미지 파일 사이즈를 확인해주세요!',
+        text: '이미지 파일은 2MB 이하로만 등록 가능합니다 :)',
+        icon: 'warning',
+        confirmButtonText: '확인',
+        confirmButtonColor: '#F1C93B',
+      });
+      return;
+    }
+  
+    if (file) {
+      formData.append('curationImage', file);
+
+      try {
+        const response = await axiosInstance.post(
+          'http://localhost:8080/curations/images/upload',
+          formData,
+          {
+            headers: {
+              Authorization: `${VITE_AUTH_KEY}`,
+            },
+          }
+        );
+        const imageUrl = response.data.imageUrl;
+
+        const editor = quillRef.current?.getEditor();
+        const range = editor?.getSelection()?.index;
+        if (range !== null && range !== undefined) {
+          editor?.insertEmbed(range, 'image', imageUrl);
+        }
+        return { ...response, success: true };
+      } catch (error) {
+        console.error(error);
       }
-    };
+    }
   };
 
   const modules = useMemo(
@@ -73,28 +89,76 @@ const QuillEditor = memo(({ quillRef, contentValue, setContentValue }: QuillEdit
 
   const formats = ['header', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'image'];
 
+  useEffect(() => {
+    const editor = quillRef.current?.getEditor();
+    if (editor) {
+      const handlePaste = (e: React.ClipboardEvent<HTMLElement>) => {
+        const clipboardData = e.clipboardData as DataTransfer;
+        if (clipboardData.types.includes('Files')) {
+          e.preventDefault();
+          customAlert({
+            title: '이미지 붙여넣기는 지원되지 않아요',
+            text: '이미지 버튼을 눌러 업로드 해주세요',
+            icon: 'warning',
+            confirmButtonText: '확인',
+            confirmButtonColor: '#F1C93B',
+          });
+        } else {
+          const textData = clipboardData.getData('text/plain');
+          if (textData) {
+            const selection = editor.getSelection();
+            if (selection) {
+              e.preventDefault();
+            }
+          }
+        }
+      };
+
+      editor.clipboard.addMatcher('br', (node, delta) => {
+        return delta;
+      });
+
+      editor.root.addEventListener('paste', handlePaste as unknown as EventListener);
+
+      return () => {
+        editor.root.removeEventListener('paste', handlePaste as unknown as EventListener);
+      };
+    }
+  }, [quillRef, setContentValue]);
+  
   return (
     <>
+      <style>
+        {`
+          .ql-toolbar {
+            border: none !important;
+            border-radius: 0.3rem 0.3rem 0rem 0rem;
+            background-color: #f8f7f7;
+          }
+          .ql-container.ql-snow {
+            border: none !important;
+            border-radius: 0rem 0rem 0.3rem 0.3rem;
+
+            background-color: #f8f7f7;
+            height: 18.75rem;
+          }
+          .ql-editor .ql-placeholder {
+            font-style: normal;
+          }
+        `}
+      </style>
       <ReactQuill
-        ref={(element) => {
+        ref={(element: ReactQuill | null) => {
           if (element !== null) {
             quillRef.current = element;
           }
         }}
-        value={contentValue}
-        onChange={setContentValue}
         modules={modules}
         formats={formats}
-        theme="bubble"
+        theme="snow"
         placeholder="큐레이션의 내용을 입력해 주세요"
-        style={{
-          fontWeight: '100',
-          marginBottom: '0.3rem',
-          backgroundColor: '#f8f7f7',
-          border: 'none',
-          borderRadius: '0.3rem',
-          height: '18.75rem',
-        }}
+        value={contentValue}
+        onChange={setContentValue}
       />
     </>
   );
