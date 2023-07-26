3ac3f70032f9c937e8eb6d96c6570fc69f353b93
Kyungju <wwmix@naver.com>
2023-07-27 00:33:20 +0900

    feat: Unhandled Exception 슬랙 연동

--- server/src/main/java/com/seb_main_004/whosbook/advice/GlobalExceptionAdvice.java
+++ server/src/main/java/com/seb_main_004/whosbook/advice/GlobalExceptionAdvice.java
@@ -2,6 +2,11 @@ package com.seb_main_004.whosbook.advice;
 
 import com.seb_main_004.whosbook.exception.BusinessLogicException;
 import com.seb_main_004.whosbook.response.ErrorResponse;
+import com.slack.api.Slack;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.Field;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,10 +17,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static com.slack.api.webhook.WebhookPayloads.payload;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionAdvice {
+    // UnHandled Exception을 슬랙으로 연동하기 위한 설정
+    private final Slack slackClient = Slack.getInstance();
+    @Value("${slack.webhook.url}")
+    private String webhookUrl;
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -77,16 +95,50 @@ public class GlobalExceptionAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleException(Exception e) {
-        System.out.println(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        System.out.println(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+    public ErrorResponse handleException(Exception e, HttpServletRequest request) {
+        log.error("UnhandledException: {} {} errMessage={}\n",
+                request.getMethod(),
+                request.getRequestURI(),
+                e.getMessage());
+
+        sendSlackAlertErrorLog(e, request);
 
         final ErrorResponse response = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
 
         return response;
     }
 
+    // 슬랙 알림을 보내는 메서드
+    private void sendSlackAlertErrorLog(Exception e, HttpServletRequest request) {
+        try {
+            slackClient.send(webhookUrl, payload(p -> p
+                    .text("서버 에러가 발생했습니다! 즉각확인 요망")
+                    .attachments(List.of(generateSlackAttachment(e, request)))));
+        } catch (IOException slackError) {
+            log.debug("Slack 통신 중 에러 발생 : {}", slackError.getMessage());
+        }
+    }
 
+    // attachments 생성메서드 : 슬랙의 메세지를 풍성하게 꾸며주는 역할
+    private Attachment generateSlackAttachment(Exception e, HttpServletRequest request) {
+        String requestTime = DateTimeFormatter.ofPattern("yyy-MM-dd HH:mm:ss.SSS").format(LocalDateTime.now());
+        String xffHeader = request.getHeader("X-FORWARDED-FOR");
+        return Attachment.builder()
+                .color("ff0000")
+                .title(requestTime + "발생 에러 로그")
+                .fields(List.of(
+                        generateSlackField("Request IP", xffHeader == null ? request.getRemoteAddr() : xffHeader),
+                        generateSlackField("Request URL", request.getRequestURI() + " " + request.getMethod()),
+                        generateSlackField("Error Message", e.getMessage())
+                )).build();
+    }
 
+    private Field generateSlackField(String title, String value) {
+        return Field.builder()
+                .title(title)
+                .value(value)
+                .valueShortEnough(false)
+                .build();
+    }
 
 }
